6497cb250ba5543bb510cc1793b63597508e65e2
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-09-09 17:24:40 +0400

    add get methods in rest

--- src/main/java/com/egs/hibernate/rest/UserResource.java
+++ src/main/java/com/egs/hibernate/rest/UserResource.java
@@ -1,16 +1,17 @@
 package com.egs.hibernate.rest;
 
 
+import com.egs.hibernate.dto.UserDTO;
 import com.egs.hibernate.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user/")
@@ -18,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "User Resource", description = "The User API with documentation annotations")
 public class UserResource {
     private final UserService userService;
-
 
     @Operation(summary = "Generate users")
     @ApiResponses(value = {
@@ -28,4 +28,19 @@ public class UserResource {
         userService.generateUsers(count);
     }
 
+    @Operation(summary = "Show users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users list have been successfully shown")})
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @Operation(summary = "Show users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users list have been successfully shown")})
+    @GetMapping("{columnName}")
+    public ResponseEntity<List<UserDTO>> filterUsers(@PathVariable String columnName){
+        return ResponseEntity.ok(userService.usersFilter(columnName));
+    }
 }
