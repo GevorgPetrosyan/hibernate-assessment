40ad169fedcdca03569c8c465eb1ea511b11a5dd
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-09-09 11:31:58 +0400

    add findAll()

--- src/main/java/com/egs/hibernate/repository/UserRepository.java
+++ src/main/java/com/egs/hibernate/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.egs.hibernate.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findFirstByOrderByUsernameDesc();
+    List<User> findAll();
 }
