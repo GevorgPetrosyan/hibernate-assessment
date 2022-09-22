f8a41004dcc93ac5753d20842a4f2578cb10ae8d
Gug234 <gugyeghiazaryan@gmail.com>
2022-09-22 10:33:14 +0400

    Task #3 and Task #4

--- src/main/java/com/egs/hibernate/repository/CountryRepository.java
+++ src/main/java/com/egs/hibernate/repository/CountryRepository.java
@@ -1,7 +1,6 @@
 package com.egs.hibernate.repository;
 
 import com.egs.hibernate.entity.Country;
-import com.egs.hibernate.response.CountryCodeResponse;
 import com.egs.hibernate.response.ResponseCountry;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
