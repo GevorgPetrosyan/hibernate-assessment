098407c649377c2d57d1ead718fc83f36a10ea52
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-10-19 18:54:02 +0400

    clean up

--- src/main/java/com/egs/hibernate/service/impl/UserServiceImpl.java
+++ src/main/java/com/egs/hibernate/service/impl/UserServiceImpl.java
@@ -23,8 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
